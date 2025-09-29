23eede05be8e3c3fa3c42e2660c79c6d4ea74bb5
Anna Oleks <annaoleks88@gmail.com>
2025-09-29 17:07:41 +0300

    wordCount

--- src/functions.js
+++ src/functions.js
@@ -2,4 +2,24 @@ function sum(a, b) {
     return a + b;
 }
 
-module.exports = { sum };
+function isPalindrome(str) {
+    let reverseStr = str.split("").reverse().join("").toLowerCase();
+    if (str.toLowerCase() == reverseStr) {
+        return true;
+    };
+    return false;
+}
+
+function isStrongPassword(password) {
+    let hasLetter = /[A-Za-z]/.test(password);
+    let hasNumber = /[0-9]/.test(password);
+    const hasSpecialChar = /[!@#$]/.test(password);
+
+    return password.length >= 16 && hasLetter && hasNumber && hasSpecialChar;
+}
+
+function wordCount(str) {
+    return str.trim().split(" ").length;
+}
+
+module.exports = { sum, isPalindrome, isStrongPassword, wordCount };
--- tests/functions.test.js
+++ tests/functions.test.js
@@ -1,7 +1,62 @@
-let { sum } = require('../src/functions');
+let { sum, isPalindrome, isStrongPassword, wordCount } = require('../src/functions');
 
 describe("sum function", () => {
     test("sum two numbers", () => {
-        expect(sum(2, 2)).toBe(4);
+        expect(sum(2, 2)).toBe(4)
+    })
+
+    test("work with null", () => {
+        expect(sum(0, 0)).toBe(0)
+    })
+})
+
+describe("Check if a string is palindrome", () => {
+    let strArray = ["Anna", "Mom", "Civic", "Kayak", "Level", "Madam", "Radar"];
+    strArray.forEach(elem => {
+        test(`${elem} is a palindrome`, () => {
+            expect(isPalindrome(elem)).toBe(true)
+        });
     });
 });
+
+describe ("Is strong password", () => {
+    let passArray =
+    ["Strongqwerty!@#123",
+    "AnotherSrtongPassword$456",
+    "YetanotherStrong1!"]
+
+    passArray.forEach(elem => {
+        test(`${elem} is a strong password`, () => {
+            expect(isStrongPassword(elem)).toBe(true)
+        });
+    });
+});
+
+describe("Is weak password", () => {
+    let passArray =
+    ["123",
+    "Anja",
+    "DorisOceana!"]
+
+    passArray.forEach(elem => {
+        test(`${elem} is a weak password`, () => {
+            expect(isStrongPassword(elem)).toBe(false)
+        });
+    });
+});
+
+describe("Count words in a string", () => {
+    let strArray = [
+        ["This is a test sentence", 5],
+        ["One two three", 3],
+        ["Count words in a string", 5]]
+    strArray.forEach(elem => {
+        test(`"${elem[0]}" has ${elem[1]} words`, () => {
+            expect(wordCount(elem[0])).toBe(elem[1])
+        });
+    })
+})
+
+// toBeGreaterThan
+// toBeLessThan
+// toBeGreaterThanOrEqual
